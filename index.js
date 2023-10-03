5e4b714abaa03fe527837ea4b8d6ac1d35e60d06
Ali Iqbal <aliiqbal436@gmail.com>
2023-10-03 16:09:09 +0500

    completed

--- index.js
+++ index.js
@@ -1,71 +1,7 @@
-// "use strict";
-
-// var http = require("http");
-// var Unblocker = require("unblocker");
-
-// var unblocker = Unblocker({});
-
-// var server = http
-//   .createServer(function (req, res) {
-//     // first let unblocker try to handle the requests
-//     unblocker(req, res, function (err) {
-//       // this callback will be fired for any request that unblocker does not serve
-//       var headers = { "content-type": "text/plain" };
-//       if (err) {
-//         res.writeHead(500, headers);
-//         return res.end(err.stack || err);
-//       }
-//       if (req.url == "/") {
-//         res.writeHead(200, headers);
-//         return res.end(
-//           "Use the format http://thissite.com/proxy/http://site-i-want.com/ to access the proxy."
-//         );
-//       } else {
-//         res.writeHead(404, headers);
-//         return res.end("Error 404: file not found.");
-//       }
-//     });
-//   })
-//   .listen(8080);
-
-// // allow unblocker to proxy websockets
-// server.on("upgrade", unblocker.onUpgrade);
-
-// console.log("proxy server live at http://localhost:8080/");
-
-// const puppeteer = require('puppeteer');
-// const express = require('express');
-// const app = express();
-// const port = 3000;
-
-// app.get('/', async (req, res) => {
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-
-//   // Navigate to the speedtest.net website
-//   await page.goto('https://www.speedtest.net/');
-
-//   // Wait for the speed test to complete (adjust the selector if needed)
-// //   await page.waitForSelector('.result-data .result-data-large .download-speed');
-
-//   // Get the HTML of the speed test result
-//   const html = await page.content();
-
-//   // Send the HTML response to the client
-//   res.send(html);
-
-//   await browser.close();
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server is running on http://localhost:${port}`);
-// });
-
 const puppeteer = require("puppeteer");
 const express = require("express");
 
 const app = express();
-const port = 3000;
 
 // Handle the search request and navigate to the specified URL
 app.get("/", async (req, res) => {
@@ -97,6 +33,4 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Proxy browser server is running on http://localhost:${port}`);
-});
+app.listen();
