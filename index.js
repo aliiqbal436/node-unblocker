979c57e5b7ed458069040debe08c0f0036c24d35
Ali Iqbal <aliiqbal436@gmail.com>
2023-10-05 23:45:39 +0500

    added socket work

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 const puppeteer = require("puppeteer");
 const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
 
-// Handle the search request and navigate to the specified URL
-app.get("/", async (_req, res) => {
-  // Launch Chromium
-  const browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    headless: "new",
-    executablePath: "/usr/bin/chromium-browser",
+const corsOptions = {
+  origin: "http://localhost:3000", // Replace with your React app's origin
+  methods: ["GET", "POST"], // Add other methods if needed
+};
+
+app.use(cors(corsOptions));
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000"
+  }
+});
+
+const runSpeedTest = async () => {
+    const browser = await puppeteer.launch({
+    // args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    headless: false,
+    // executablePath: "/usr/bin/chromium-browser",
   });
   const page = await browser.newPage();
   try {
     // Navigate to the specified URL
     await page.goto(`https://www.google.com/search?q=speed+test`);
-    // await page.waitForNavigation({ waitUntil: 'networkidle0' });
     const cookiesModalSelector = "#L2AGLb";
     const modalPresent = await page
       .waitForSelector(cookiesModalSelector, {
@@ -50,16 +64,33 @@ app.get("/", async (_req, res) => {
     await page.waitForSelector(
       "#knowledge-verticals-internetspeedtest__upload"
     );
-    await page.waitForTimeout(30000);
+    await page.waitForTimeout(50000);
 
     const html = await page.content();
     await browser.close();
-
-    res.send(html);
+    io.emit("testResult", html);
   } catch (error) {
     console.log("error.message ====", error.message);
-    res.status(500).send(error.message);
   }
+}
+
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("runTest", () => {
+    // Broadcast the message to all connected clients
+    runSpeedTest()
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
 });
 
-app.listen(3007);
+// Handle the search request and navigate to the specified URL
+// app.get("/speed-test", async (_req, res) => {
+//   // Launch Chromium
+
+// });
+httpServer.listen(3007);
